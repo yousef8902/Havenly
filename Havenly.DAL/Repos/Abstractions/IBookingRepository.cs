c63d8c04c326d90b72a6a0663e115fecc583b026
mariam1617 <mariam.abd6606@gmail.com>
2026-08-20 16:00:53 +0300

    added BookingREpo with (getActiveBooking, AddBooking, GettALLQueryble )

---
+++ Havenly.DAL/Repos/Abstractions/IBookingRepository.cs
@@ -0,0 +1,15 @@
+﻿using Havenly.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Havenly.DAL.Repos.Abstractions
+{
+   public interface IBookingRepository
+    {
+        public Task<IEnumerable<Booking>> GetActiveBookingsForListingAsync(long listingId, DateTime checkIn, DateTime checkOut);
+        public Task AddBooking(Booking B);
+
+        public IQueryable<Booking> GetAll();
+    }
+}
---
+++ Havenly.DAL/Repos/Implementations/BookingRepository.cs
@@ -0,0 +1,43 @@
+﻿using Havenly.DAL.Database;
+using Havenly.DAL.Entities;
+using Havenly.DAL.Enums;
+using Havenly.DAL.Repos.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Havenly.DAL.Repos.Implementations
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly HavenlyDbContext _context;
+
+        public BookingRepository(HavenlyDbContext context)
+        {
+            _context = context;
+        }
+
+        // Fetches only Pending and Approved bookings that overlap with the requested dates
+        public async Task<IEnumerable<Booking>> GetActiveBookingsForListingAsync(long listingId, DateTime checkIn, DateTime checkOut)
+        {
+            var activeStatuses = new[] { BookingStatus.Pending, BookingStatus.Approved };
+
+            return await _context.Bookings
+                .AsNoTracking()
+                .Where(b => b.ListingID == listingId
+                         && activeStatuses.Contains(b.Status)
+                         && b.CheckIn < checkOut
+                         && b.CheckOut > checkIn)
+                .ToListAsync();
+        }
+
+        public async Task AddBooking(Booking booking)
+        {
+            await _context.Bookings.AddAsync(booking);
+        }
+
+        public IQueryable<Booking> GetAll()
+        {
+            return _context.Bookings.AsQueryable();
+        }
+    }
+}
